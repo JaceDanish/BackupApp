5f103ec1e18d92082123ab95153b17704ddee76c
JaceDanish <47661433+JaceDanish@users.noreply.github.com>
2021-02-26 13:11:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace BackupApp
+{
+    class Program
+    {
+        private static int fileCount = 1;
+        static void Main(string[] args)
+        {
+            String sourceDir = Directory.GetCurrentDirectory();
+            String targetDir;
+            
+            if (args.Length == 1 && Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Destination folder found");
+                targetDir = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Destination folder not found");
+                Console.WriteLine("Usage example: BackupApp D:\\directory");
+                return;
+            }
+            BackUpRec(targetDir, sourceDir);
+
+            Console.WriteLine("Back up complete");
+        }
+
+        static void BackUpRec(String targetDir, String sourceDir)
+        {
+            //file contains full path name
+            foreach (String file in Directory.GetFiles(sourceDir))
+            {
+                String[] path = file.Split('\\');
+                byte[] b = File.ReadAllBytes(file);
+
+                File.WriteAllBytes($"{targetDir}\\{path[path.Length - 1]}", b);
+                Console.WriteLine(fileCount++);
+            }
+
+            //dirs contain full path name
+            foreach (String dirs in Directory.GetDirectories(sourceDir))
+            {
+                String[] path = dirs.Split('\\');
+                String dir = path[path.Length - 1];
+
+                if (!Directory.Exists($"{targetDir}\\{dir}"))
+                {
+                    Directory.CreateDirectory($"{targetDir}\\{dir}");
+                }
+                BackUpRec($"{targetDir}\\{dir}", $"{sourceDir}\\{dir}");
+            }
+        }
+    }
+}
